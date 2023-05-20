212d7dd246040f974a537141fb572b0d0aee7618
sunny0105yoko <ec2-user@ip-172-31-26-60.ap-northeast-1.compute.internal>
2023-05-20 15:53:13 +0000

    [update]kadai

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,10 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
       if @book.save
+        flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
+      else
+      @books = Book.all
       end
   end
 
@@ -32,7 +35,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book.update(book_params)
     if @book.save
-      flash[:notice] = "Book was successfully updated."
+      flash[:notice] = "You have updated book successfully."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
