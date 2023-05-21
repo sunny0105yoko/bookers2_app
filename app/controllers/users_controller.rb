4315deb0167cb6da938995fe5b7eb69aeb584547
sunny0105yoko <ec2-user@ip-172-31-26-60.ap-northeast-1.compute.internal>
2023-05-21 14:10:52 +0000

    [updated]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,21 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def show
     @user =User.find(params[:id])
     @books =@user.books
     @book = Book.new
   end
-  
+
   def index
     @users =User.all
     @book = Book.new
   end
-  
+
   def edit
-    @user =User.find(params[:id])
+    @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -25,13 +27,13 @@ class UsersController < ApplicationController
   end
 
 private
-  
+
   def user_params
     #params.require(:user).permit(:name, :email, :password)
     params.require(:user).permit(:name, :book, :profile_image, :introduction)
   end
 
-  def is matching_login_user
+  def is_matching_login_user
     user = User.find(params[:id])
     unless user.id == current_user.id
       redirect_to book_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  get 'homes/about' => 'homes#about',as:'about'
+  get 'home/about' => 'homes#about',as:'about'
   #get 'users/show' => 'users#show' ,as:'user_show'
   #get 'users/edit'
   #get 'books/edit'
